ab152f47f318797cb2735bbafa65cd2b05493eba
Carlos Lima <150730209+carlos-lima-dev@users.noreply.github.com>
2024-08-13 14:15:18 +0100

    Update scheduler.ts

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -17,7 +17,7 @@ const scheduleDailyCleanup = () => {
     }
   });
 
-  cron.schedule("00 13 * * *", async () => {
+  cron.schedule("18 14 * * *", async () => {
     console.log("Running daily reminder job at", new Date());
     try {
       const tomorrow = new Date();
